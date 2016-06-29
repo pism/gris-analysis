413d08a2d68410e0466a02820415a68adda95986
Andy Aschwanden <aaschwanden@alaska.edu>
2016-06-28 18:51:08 -0800

    more analysis stuff but needs refactoring

--- holocene/speed_change.sh
+++ holocene/speed_change.sh
@@ -2,15 +2,27 @@
 
 # Copyright (C) 2016 Andy Aschwanden
 
-if [ $# -gt 0 ] ; then  # if user says "paramspawn.sh 8" then NN = 8
+if [ $# -gt 0 ] ; then
   infile="$1"
 fi
 
-outfile=velsurf_mag_modern_9ka_$infile
-ncra -O -v x,y,mapping,velsurf_mag -d time,-91,-1 $infile tmp_9ka.nc
-ncks -O -v x,y,mapping,velsurf_mag -d time,-1 $infile tmp_modern.nc
-ncdiff -O tmp_modern.nc tmp_9ka.nc $outfile
+outfile=holocene/lgm_modern_$infile
+ncks -O -d time,-201,-1 $infile $outfile
+ncrename -v nuH[0],nuH0 -v nuH[1],nuH1 $outfile
+ncap2 -4 -L 3 -O -s "nu=sqrt(nuH0^2+nuH1^2);" $outfile $outfile
+
+exit
+
+outfile=holocene/hc_modern_$infile
+ncks -O -d time,-121,-1 $infile $outfile
+ncrename -v nuH[0],nuH0 -v nuH[1],nuH1 $outfile
+ncap2 -4 -L 3 -O -s "nu=sqrt(nuH0^2+nuH1^2);" $outfile $outfile
+
+outfile=holocene/velsurf_mag_modern_9ka_$infile
+ncra -O -v x,y,mapping,velsurf_mag -d time,-91,-1 $infile tmp_9ka_$infile
+ncks -O -v x,y,mapping,velsurf_mag -d time,-1 $infile tmp_modern_$infile
+ncdiff -O tmp_modern_$infile tmp_9ka_$infilec $outfile
 ncks -A -v mask -d time,-1 $infile $outfile
 ncap2 -O -s "where(mask!=2) velsurf_mag=-2e9;" $outfile $outfile
 ncatted -a valid_min,velsurf_mag,d,, $outfile
-rm tmp_9ka.nc tmp_modern.nc
+rm tmp_9ka_$infile tmp_modern_$infile
