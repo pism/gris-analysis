19f780ba4f243f047395e6e8d9c33c5ef41e20f8
Andy Aschwanden <aaschwanden@alaska.edu>
2016-09-12 12:48:18 -0800

    added merged basins

--- holocene/speed_change.sh
+++ holocene/speed_change.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
 # Copyright (C) 2016 Andy Aschwanden
+set -x -e
 
 if [ $# -gt 0 ] ; then
   infile="$1"
 fi
+
+
+for var in surface_mass_balance_average velsurf_mag; do
+#for var in surface_mass_balance_average; do
+    outfile=holocene/${var}_modern_9ka_$infile
+    outfilerel=holocene/rel_${var}_modern_9ka_$infile
+    ncra -O -v x,y,mapping,${var} -d time,-91,-1 $infile tmp_9ka_$infile
+    ncks -O -v x,y,mapping,${var} -d time,-1 $infile tmp_modern_$infile
+    ncdiff -O tmp_modern_$infile tmp_9ka_$infile $outfile
+    ncks -A -v mask -d time,-1 $infile $outfile
+    ncap2 -O -s "where(mask!=2) ${var}=-2e9;" $outfile $outfile
+    ncatted -a valid_min,${var},d,, $outfile
+    cdo div -selvar,$var $outfile -selvar,$var tmp_9ka_$infile $outfilerel
+    ncks -A -v x,y,mapping $infile $outfilerel
+    ncatted -a units,$var,d,, -a grid_mapping,$var,o,c,"mapping" $outfilerel
+    rm tmp_9ka_$infile tmp_modern_$infile
+done
+
+exit
 
 outfile=holocene/lgm_modern_$infile
 ncks -O -d time,-201,-1 $infile $outfile
 ncrename -v nuH[0],nuH0 -v nuH[1],nuH1 $outfile
 ncap2 -4 -L 3 -O -s "nu=sqrt(nuH0^2+nuH1^2);" $outfile $outfile
 
+infile2=$outfile
+outfile=holocene/hc_modern_$infile
+ncks -O -d time,-121,-1 $infile2 $outfile
+
 exit
 
-outfile=holocene/hc_modern_$infile
-ncks -O -d time,-121,-1 $infile $outfile
-ncrename -v nuH[0],nuH0 -v nuH[1],nuH1 $outfile
-ncap2 -4 -L 3 -O -s "nu=sqrt(nuH0^2+nuH1^2);" $outfile $outfile
-
-outfile=holocene/velsurf_mag_modern_9ka_$infile
-ncra -O -v x,y,mapping,velsurf_mag -d time,-91,-1 $infile tmp_9ka_$infile
-ncks -O -v x,y,mapping,velsurf_mag -d time,-1 $infile tmp_modern_$infile
-ncdiff -O tmp_modern_$infile tmp_9ka_$infilec $outfile
-ncks -A -v mask -d time,-1 $infile $outfile
-ncap2 -O -s "where(mask!=2) velsurf_mag=-2e9;" $outfile $outfile
-ncatted -a valid_min,velsurf_mag,d,, $outfile
-rm tmp_9ka_$infile tmp_modern_$infile
--- scripts/test-ocgis.py
+++ scripts/test-ocgis.py
@@ -5,6 +5,7 @@
 from argparse import ArgumentParser
 import tempfile
 import ocgis
+import os
 
 
 # set up the option parser
@@ -13,10 +14,16 @@ parser.description = "Generating scripts for prognostic simulations."
 parser.add_argument("FILE", nargs=1)
 parser.add_argument("--shape_file", dest="shape_file",
                     help="Path to shape file with basins", default=None)
+parser.add_argument("--o_dir", dest="odir",
+                    help="output directory", default='test')
 
 options = parser.parse_args()
 filename = options.FILE[0]
 SHAPEFILE_PATH = options.shape_file
+
+odir = options.odir
+if not os.path.isdir(odir):
+    os.mkdir(odir)
 
 ocgis.env.OVERWRITE = True
 
@@ -27,22 +34,20 @@ URI = filename
 VARIABLE = None
 VARIABLE = 'discharge_flux'
 
-## this is the path to the shapefile containing state boundaries
-
-## write data to a new temporary directory for each script start
-DIR_OUTPUT = None
+# Create filename
+savename=filename[0:len(filename)-3] 
 
 ## set the output format to convert to
-OUTPUT_FORMAT = 'nc'
+output_format = 'nc'
 
 
 ## we can either subset the data by a geometry from a shapefile, or convert to
 ## geojson for the entire spatial domain. there are other options here (i.e. a
 ## bounding box for tiling or a Shapely geometry).
 GEOM = SHAPEFILE_PATH
-GEOM = None
+#GEOM = None
 
-basin = 7.1
+basins = range(1, 9)
 
 ## connect to the dataset and load the data as a field object. this will be used
 ## to iterate over time coordinates during the conversion step.
@@ -53,30 +58,30 @@ basin = 7.1
 ## geometry's UGID inside the shapefile. shapefile are required to have this
 ## identifier at this time as a full attribute search is not enabled. this code
 ## searches for TX to find the UGID associated with that state.
-if GEOM is None:
-    select_ugid = None
-else:
-    select_geom = filter(lambda x: x['properties']['basin'] == basin,
-                         ocgis.GeomCabinetIterator(path=SHAPEFILE_PATH))
-    ## this argument must always come in as a list
-    select_ugid = [select_geom[0]['properties']['UGID']]
     
-## USE OCGIS OPERATIONS ########################################################
 
 
 ## this again is the target dataset with a time range for subsetting now
 #rd = ocgis.RequestDataset(uri=URI,variable=VARIABLE,time_range=[centroid,centroid])
 rd = ocgis.RequestDataset(uri=URI, variable=VARIABLE)
-prefix = 'basin_{basin}'.format(basin=basin)
-## parameterize the operations to be performed on the target dataset
-ops = ocgis.OcgOperations(dataset=rd,
-                          geom=SHAPEFILE_PATH,
-                          aggregate=False,
-                          snippet=True,
-                          select_ugid=select_ugid,
-                          output_format=OUTPUT_FORMAT,
-                          prefix=prefix,
-                          dir_output=DIR_OUTPUT)
-ret = ops.execute()
-print('path to output file: {0}'.format(ret))
+for basin in basins:
+    if GEOM is None:
+        select_ugid = None
+    else:
+        select_geom = filter(lambda x: x['properties']['basin'] == basin,
+                             ocgis.GeomCabinetIterator(path=SHAPEFILE_PATH))
+        ## this argument must always come in as a list
+        select_ugid = [select_geom[0]['properties']['UGID']]
+    prefix = 'basin_{basin}_{savename}'.format(basin=basin, savename=savename)
+    ## parameterize the operations to be performed on the target dataset
+    ops = ocgis.OcgOperations(dataset=rd,
+                              geom=SHAPEFILE_PATH,
+                              aggregate=False,
+                              snippet=True,
+                              select_ugid=select_ugid,
+                              output_format=output_format,
+                              prefix=prefix,
+                              dir_output=odir)
+    ret = ops.execute()
+    print('path to output file: {0}'.format(ret))
 
