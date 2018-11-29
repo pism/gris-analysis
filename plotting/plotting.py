963128f733794af34997e78004c320c7d208d4ae
Andy Aschwanden <aaschwanden@alaska.edu>
2018-11-28 18:07:06 -0900

    AGU movie stuff added

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -229,7 +229,9 @@ parser.add_argument(
         "ens_mass",
         "ctrl_mass",
         "forcing_mass",
+        "mass_d",
         "profile",
+        "profile_anim",
         "flux_partitioning",
         "basin_flux_partitioning",
         "ctrl_mass_anim",
@@ -544,7 +546,140 @@ def plot_profile_ts_combined():
 
     nc.close()
 
+def plot_profile_ts_animation():
 
+    try:
+        cmap = getattr(plt.cm, my_colormap)
+    except:
+        # import and convert colormap
+        cdict = gmtColormap(my_colormap)
+        cmap = mpl.colors.LinearSegmentedColormap("my_colormap", cdict, 1024)
+
+    mcm = cm = plt.get_cmap(cmap)
+
+    nc = NC(ifiles[0], "r")
+    profile_names = nc.variables["profile_name"][:]
+    profile = u"Upernavik Isstr\xf8m S"
+    k = 1
+
+    print((u"Processing {} profile".format(profile)))
+
+    profile_iunits = nc.variables["profile"].units
+    profile_ounits = "km"
+    profile_vals = nc.variables["profile"][k, :]
+    profile_vals = unit_converter(profile_vals, profile_iunits, profile_ounits)
+
+    t_var = nc.variables["time"][:]
+    date = np.arange(start_year, start_year + (len(t_var[:]) + 1), step)
+    ma = np.where(date == time_bounds[0])[0][0]
+    me = np.where(date == time_bounds[1])[0][0]
+
+    plot_times = np.arange(ma, me + 1, step)
+
+    for t in plot_times:
+
+        fig, ax = plt.subplots(3, 1, sharex="col", figsize=[3, 2])
+        fig.subplots_adjust(hspace=0.15, wspace=0.05)
+
+        cNorm = colors.Normalize(vmin=time_bounds[0], vmax=time_bounds[1])
+        scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=mcm)
+
+        # plot previous fluxes
+        for ts in range(plot_times[0], t):
+            colorVal = scalarMap.to_rgba(date[t])
+            speed_vals = nc.variables["velsurf_mag"][k, ts, :]
+            mask = speed_vals < 1
+            speed_vals = np.ma.array(speed_vals, mask=mask)
+            topg_vals = nc.variables["topg"][k, ts, :]
+            thk_vals = nc.variables["thk"][k, ts, :]
+            usurf_vals = nc.variables["usurf"][k, ts, :]
+            thk_mask = thk_vals <= 20
+            thk_vals = np.ma.array(thk_vals, mask=thk_mask)
+            usurf_mask = usurf_vals < 100
+            usurf_mask = np.logical_or((usurf_vals < topg_vals), thk_mask)
+            usurf_vals = np.ma.array(usurf_vals, mask=usurf_mask)
+            ax[0].plot(profile_vals, speed_vals * thk_vals / 1e6, color='0.5', linewidth=0.1)
+            ax[1].plot(profile_vals, speed_vals, color='0.5', linewidth=0.1)
+
+        colorVal = scalarMap.to_rgba(date[t])
+        speed_vals = nc.variables["velsurf_mag"][k, t, :]
+        mask = speed_vals < 1
+        speed_vals = np.ma.array(speed_vals, mask=mask)
+        topg_vals = nc.variables["topg"][k, t, :]
+        thk_vals = nc.variables["thk"][k, t, :]
+        usurf_vals = nc.variables["usurf"][k, t, :]
+        thk_mask = thk_vals <= 20
+        thk_vals = np.ma.array(thk_vals, mask=thk_mask)
+        usurf_mask = usurf_vals < 100
+        usurf_mask = np.logical_or((usurf_vals < topg_vals), thk_mask)
+        usurf_vals = np.ma.array(usurf_vals, mask=usurf_mask)
+        ax[0].plot(profile_vals, speed_vals * thk_vals / 1e6, color=colorVal, linewidth=0.6)
+        ax[1].plot(profile_vals, speed_vals, color=colorVal, linewidth=0.6)
+        try:
+            ax[2].plot(profile_vals, usurf_vals, color='k', linewidth=0.3)
+            bottom_vals = np.maximum(usurf_vals - thk_vals, topg_vals)
+            ax[2].plot(profile_vals, np.ma.array(bottom_vals, mask=thk_mask), color='k', linewidth=0.3)
+            ax[2].fill_between(profile_vals, np.ma.array(bottom_vals, mask=thk_mask), usurf_vals, color=colorVal, linewidth=0.3)
+            ax[2].fill_between(profile_vals, bottom_vals, usurf_vals, color=colorVal, linewidth=0.3)
+        except:
+            pass
+        if t == plot_times[-1]:
+            ax[2].plot(profile_vals, topg_vals, color="k", linewidth=0.3)
+            try:
+                ax[2].plot(profile_vals, usurf_vals, color="k", linewidth=0.1)
+                bottom_vals = np.maximum(usurf_vals - thk_vals, topg_vals)
+                ax[2].plot(profile_vals, np.ma.array(bottom_vals, mask=thk_mask), color="k", linewidth=0.1)
+            except:
+                pass
+
+        xmin, xmax = ax[1].get_xlim()
+        ymin, ymax = ax[1].get_ylim()
+
+        ymin = -1200
+        ax[0].set_ylabel("Flux\n (km$^{\mathregular{2}}$ yr$^{\mathregular{-1}}$)", multialignment="center")
+        ax[1].set_ylabel("Speed\n (m yr$^{\mathregular{-1}}$)", multialignment="center")
+        tz = ax[2].fill_between([0, xmax], [ymin, ymin], color="#c6dbef", linewidth=0)
+        tz.set_zorder(-1)
+        tz = ax[2].fill_between(profile_vals, topg_vals * 0 + ymin, topg_vals, color="#fdbe85", linewidth=0)
+        ax[2].set_ylabel("Altitude\n (masl)", multialignment="center")
+        tz = ax[2].axhline(profile_vals[0], linestyle="solid", color="k", linewidth=0.3)
+        tz.set_zorder(-1)
+        ax[2].set_xlabel("distance ({})".format(profile_ounits))
+
+        ax[2].set_xlim(np.nanmin(profile_vals), np.nanmax(profile_vals))
+        ax[2].set_ylim(np.nanmin(topg_vals))
+        ax[2].yaxis.set_major_formatter(FormatStrFormatter("%1.0f"))
+
+        if bounds:
+            ax[1].set_ylim(bounds[0], bounds[1])
+
+        ax[0].set_ylim(0, 2.5)
+        ax[0].set_xlim(0, 80)
+        ax[1].set_xlim(0, 80)
+        ax[2].set_xlim(0, 80)
+
+        if rotate_xticks:
+            ticklabels = ax[1].get_xticklabels()
+            for tick in ticklabels:
+                tick.set_rotation(30)
+        else:
+            ticklabels = ax[1].get_xticklabels()
+            for tick in ticklabels:
+                tick.set_rotation(0)
+
+        if title is not None:
+            plt.title(title)
+
+        for out_format in out_formats:
+            out_file = outfile + "_{}_{:04d}".format(unidecode(profile).replace(" ", "_"), t) + "." + out_format
+            print("  - writing image %s ..." % out_file)
+            fig.savefig(out_file, bbox_inches="tight", dpi=out_res)
+            plt.close()
+            del fig
+
+    nc.close()
+
+    
 def plot_ctrl_mass_anim(plot_var=mass_plot_var):
 
     rcp_26_file = [f for f in ifiles if "rcp_{}".format(26) in f][0]
@@ -894,21 +1029,22 @@ def plot_les(plot_var=mass_plot_var):
             linewidth=0.2,
         )
 
-        print(("Reading RCP {} files".format(rcp)))
-        rcp_ctrl_file = [
-            f for f in ctrl_file if ("rcp_{}".format(rcp) in f) and not ("tas" in f) and ("flux_absolute" in f)
-        ]
-        cdf_ctrl = cdo.runmean(runmean_window, input=rcp_ctrl_file, returnCdf=True, options=pthreads)
-        ctrl_t = cdf_ctrl.variables["time"][:]
-        ctrl_date = np.arange(start_year + step, start_year + (len(ctrl_t[:]) + 1), step)
+        if ctrl_file is not None:
+            print(("Reading RCP {} files".format(rcp)))
+            rcp_ctrl_file = [
+                f for f in ctrl_file if ("rcp_{}".format(rcp) in f) and not ("tas" in f) and ("flux_absolute" in f)
+            ]
+            cdf_ctrl = cdo.runmean(runmean_window, input=rcp_ctrl_file, returnCdf=True, options=pthreads)
+            ctrl_t = cdf_ctrl.variables["time"][:]
+            ctrl_date = np.arange(start_year + step, start_year + (len(ctrl_t[:]) + 1), step)
 
-        ctrl_vals = cdf_ctrl.variables[plot_var][:]
-        iunits = cdf_ctrl[plot_var].units
-        ctrl_vals = -unit_converter(ctrl_vals, iunits, flux_ounits) * gt2mmSLE
+            ctrl_vals = cdf_ctrl.variables[plot_var][:]
+            iunits = cdf_ctrl[plot_var].units
+            ctrl_vals = -unit_converter(ctrl_vals, iunits, flux_ounits) * gt2mmSLE
 
-        ax[3].plot(
-            ctrl_date[:], ctrl_vals, color=rcp_col_dict[rcp], label=rcp_dict[rcp], linestyle="solid", linewidth=lw
-        )
+            ax[3].plot(
+                ctrl_date[:], ctrl_vals, color=rcp_col_dict[rcp], label=rcp_dict[rcp], linestyle="solid", linewidth=lw
+            )
 
     print("Discharge Contribution Relative")
     plot_var = "discharge_contrib"
@@ -974,7 +1110,7 @@ def plot_les(plot_var=mass_plot_var):
     ax[2].set_ylabel("$\dot M$\n(mm SLE yr$^{\mathregular{-1}}$)")
     ax[3].set_ylabel("$\dot D_{\dot M}$ \n(mm SLE yr$^{\mathregular{-1}}$)")
     ax[4].set_ylabel("$\dot D_{\%}$ (%)")
-    ax[3].set_xlabel("Year")
+    ax[4].set_xlabel("Year")
 
     add_inner_title(ax[0], "a", "upper left")
     add_inner_title(ax[1], "b", "upper left")
@@ -1153,6 +1289,151 @@ def plot_forcing_mass(plot_var=mass_plot_var):
         out_file = outfile + "_ctrl." + out_format
         print("  - writing image %s ..." % out_file)
         fig2.savefig(out_file, bbox_inches="tight", dpi=out_res)
+
+
+def plot_mass_contrib_d(plot_var=mass_plot_var):
+
+    end_years = [992, 992, 992]
+
+
+    for k, rcp in enumerate(rcp_list[::-1]):
+
+        fig, ax = plt.subplots(2, 1, sharex="col", sharey="row", figsize=[3, 2])
+        fig.subplots_adjust(hspace=0.05, wspace=0.30)
+        
+        print("Cumulative Mass")
+        plot_var = "limnsw"
+        
+        print(("Reading RCP {} files".format(rcp)))
+        rcp_files = [f for f in ifiles if ("rcp_{}".format(rcp) in f) and not ("tas" in f)]
+
+        pctl16_file = [f for f in rcp_files if "enspctl16" in f][0]
+        pctl84_file = [f for f in rcp_files if "enspctl84" in f][0]
+
+        cdf_enspctl16 = cdo.readCdf(pctl16_file)
+        cdf_enspctl84 = cdo.readCdf(pctl84_file)
+        t = cdf_enspctl16.variables["time"][:]
+
+        enspctl16 = cdf_enspctl16.variables[plot_var][:]
+        enspctl16_vals = cdf_enspctl16.variables[plot_var][:] - cdf_enspctl16.variables[plot_var][0]
+        iunits = cdf_enspctl16[plot_var].units
+        enspctl16_vals = -unit_converter(enspctl16_vals, iunits, mass_ounits) * gt2mSLE
+
+        enspctl84 = cdf_enspctl84.variables[plot_var][:]
+        enspctl84_vals = cdf_enspctl84.variables[plot_var][:] - cdf_enspctl84.variables[plot_var][0]
+        iunits = cdf_enspctl84[plot_var].units
+        enspctl84_vals = -unit_converter(enspctl84_vals, iunits, mass_ounits) * gt2mSLE
+
+        date = np.arange(start_year + step, start_year + (len(t[:]) + 1), step)
+
+        # ensemble between 16th and 84th quantile
+        ax[0].fill_between(date[:], enspctl16_vals, enspctl84_vals, color=rcp_shade_col_dict[rcp], linewidth=0)
+
+        ax[0].plot(date[:], enspctl16_vals, color=rcp_col_dict[rcp], linestyle="solid", linewidth=0.20)
+
+        ax[0].plot(date[:], enspctl84_vals, color=rcp_col_dict[rcp], linestyle="solid", linewidth=0.20)
+
+        if ctrl_file is not None:
+            rcp_ctrl_file = [f for f in ctrl_file if "rcp_{}".format(rcp) in f][0]
+
+            cdf_ctrl = cdo.readCdf(rcp_ctrl_file)
+            ctrl_t = cdf_ctrl.variables["time"][:]
+            cdf_date = np.arange(start_year + step, start_year + (len(ctrl_t[:]) + 1), step)
+
+            ctrl_vals = cdf_ctrl.variables[plot_var][:] - cdf_ctrl.variables[plot_var][0]
+            iunits = cdf_ctrl[plot_var].units
+            ctrl_vals = -unit_converter(ctrl_vals, iunits, mass_ounits) * gt2mSLE
+            ax[0].plot(cdf_date[:], ctrl_vals, color=rcp_col_dict[rcp], linestyle="solid", linewidth=lw)
+
+        print("Discharge Contribution Relative")
+        plot_var = "discharge_contrib"
+
+        rcp_files = [f for f in ifiles if ("rcp_{}".format(rcp) in f) and ("flux_percent" in f)]
+        pctl16_file = [f for f in rcp_files if "enspctl16" in f][0]
+        pctl84_file = [f for f in rcp_files if "enspctl84" in f][0]
+
+        cdf_enspctl16 = cdo.runmean(runmean_window, input=pctl16_file, returnCdf=True, options=pthreads)
+        cdf_enspctl84 = cdo.runmean(runmean_window, input=pctl84_file, returnCdf=True, options=pthreads)
+        t = cdf_enspctl16.variables["time"][:]
+
+        enspctl16 = cdf_enspctl16.variables[plot_var][:]
+        enspctl16_vals = cdf_enspctl16.variables[plot_var][:]
+
+        enspctl84 = cdf_enspctl84.variables[plot_var][:]
+        enspctl84_vals = cdf_enspctl84.variables[plot_var][:]
+        date = np.arange(start_year + step, start_year + (len(t[:]) + 1), step)
+
+        # ensemble between 16th and 84th quantile
+        ax[1].fill_between(
+            date[: end_years[k]],
+            enspctl16_vals[: end_years[k]],
+            enspctl84_vals[: end_years[k]],
+            color=rcp_shade_col_dict[rcp],
+            linewidth=0,
+        )
+
+        ax[1].plot(
+            date[: end_years[k]],
+            enspctl16_vals[: end_years[k]],
+            color=rcp_col_dict[rcp],
+            linestyle="solid",
+            linewidth=0.2,
+        )
+
+        ax[1].plot(
+            date[: end_years[k]],
+            enspctl84_vals[: end_years[k]],
+            color=rcp_col_dict[rcp],
+            linestyle="solid",
+            linewidth=0.2,
+        )
+
+        if ctrl_file is not None:
+            rcp_ctrl_file = [f for f in ctrl_file if ("rcp_{}".format(rcp) in f) and ("flux_percent" in f)][0]
+            cdf_ctrl = cdo.runmean(runmean_window, input=rcp_ctrl_file, returnCdf=True, options=pthreads)
+            ctrl_t = cdf_ctrl.variables["time"][:]
+            cdf_date = np.arange(start_year + step, start_year + (len(ctrl_t[:]) + 1), step)
+
+            ctrl_vals = cdf_ctrl.variables[plot_var][:]
+            ax[1].plot(cdf_date[:], ctrl_vals, color=rcp_col_dict[rcp], linestyle="solid", linewidth=lw)
+            
+        if do_legend:
+            legend = ax[1].legend(
+                loc="upper left", edgecolor="0", bbox_to_anchor=(0.0, 0.0, 0, 0), bbox_transform=plt.gcf().transFigure
+            )
+            legend.get_frame().set_linewidth(0.0)
+            legend.get_frame().set_alpha(0.0)
+
+        ax[0].set_ylabel("$\Delta$(GMSL)\n(m)")
+        ax[1].set_ylabel("$\dot D_{\%}$ (%)")
+
+        if time_bounds:
+            ax[1].set_xlim(time_bounds[0], time_bounds[1])
+
+        # ax[0].set_ylim(-1, 16)
+        # ax[1].set_ylim(0, 8)
+
+        ymin, ymax = ax[1].get_ylim()
+        ax[1].yaxis.set_major_formatter(FormatStrFormatter("%1.0f"))
+
+        if rotate_xticks:
+            ticklabels = ax[1].get_xticklabels()
+            for tick in ticklabels:
+                tick.set_rotation(30)
+        else:
+            ticklabels = ax[1].get_xticklabels()
+            for tick in ticklabels:
+                tick.set_rotation(0)
+
+        if title is not None:
+            plt.title(title)
+
+        # set_size(2.44, 0.86)
+
+        for out_format in out_formats:
+            out_file = outfile + "_mass_d_rcp_{}.".format(rcp) + out_format
+            print("  - writing image %s ..." % out_file)
+            fig.savefig(out_file, bbox_inches="tight", dpi=out_res)
 
 
 def plot_random_flux(plot_var=flux_plot_var):
@@ -1672,6 +1953,8 @@ if plot == "les":
     plot_les()
 elif plot == "forcing_mass":
     plot_forcing_mass(plot_var="limnsw")
+elif plot == "mass_d":
+    plot_mass_contrib_d(plot_var="limnsw")
 elif plot == "ens_mass":
     plot_ens_mass(plot_var="limnsw")
 elif plot == "ctrl_mass":
@@ -1690,3 +1973,5 @@ elif plot == "random_flux":
     plot_random_flux(plot_var="tendency_of_ice_mass_due_to_discharge")
 elif plot == "profile":
     plot_profile_ts_combined()
+elif plot == "profile_anim":
+    plot_profile_ts_animation()
